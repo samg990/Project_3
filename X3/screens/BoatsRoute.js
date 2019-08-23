02d4b9264cb6bed0b9307e2a79c77fc47a6a726e
Mo <Gabalyma@gmail.com>
2019-08-22 21:53:29 -0700

    Added Cards and changed tapbar location and names

--- X3/screens/BoatsRoute.js
+++ X3/screens/BoatsRoute.js
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
+import Cards from "../components/Cards";
 
 export default function FirstRoute() {
-  return <View style={[styles.scene, { backgroundColor: "#ff4081" }]} />;
+  return (
+    <View style={[styles.scene, { backgroundColor: "black" }]}>
+      <Cards style={{ zIndex: 0 }} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
